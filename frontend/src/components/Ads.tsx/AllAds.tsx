22d53c3dcf51e0ad86ba3201a4473a4985b6d914
Jeynox <daniel.lemos.jose1998@gmail.com>
2025-01-15 00:35:27 +0100

    :lipstick: ajout du css pour les cartes

--- frontend/src/components/Ads.tsx/AllAds.tsx
+++ frontend/src/components/Ads.tsx/AllAds.tsx
@@ -1,17 +1,45 @@
 import AdCard from "./AdCard";
 import '../../assets/components/ads.css';
 
+const Ads = [{
+    id: 1,
+    img: "https://placehold.co/100",
+    title: "Bike",
+    price: 100,
+    date: "2021-09-01",
+    address: "123 Main St"
+},
+{
+    id: 2,
+    img: "https://placehold.co/100",
+    title: "Car",
+    price: 1000,
+    date: "2021-09-01",
+    address: "123 Main St"
+},
+{
+    id: 3,
+    img: "https://placehold.co/100",
+    title: "House",
+    price: 100000,
+    date: "2021-09-01",
+    address: "123 Main St"
+}]
+
 const AllAds = () => {
     return (
         <div className="ads">
-            <AdCard
-                id={1}
-                img="https://placehold.co/200"
-                title="Bike"
-                price={100}
-                date="2021-09-01"
-                address="123 Main St"
-            />
+            {Ads.map(ad => 
+                <AdCard 
+                    key={ad.id} 
+                    img={ad.img} 
+                    title={ad.title} 
+                    price={ad.price} 
+                    address={ad.address} 
+                    date={ad.date} 
+                    id={0} 
+                />
+            )}
         </div>
     )
 }
